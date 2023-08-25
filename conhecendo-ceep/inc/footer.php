31f71349ecec871add99aff240386619c202800f
Eduardo <111794540+EduardoMachadoSouza@users.noreply.github.com>
2023-08-25 10:43:32 -0300

    Add files via upload

---
+++ conhecendo-ceep/inc/footer.php
@@ -0,0 +1,9 @@
+<footer> <div class ="social "> 
+    
+<li><a href="https://www.facebook.com">.</a></li>
+<li><a href="https://www.Instagram.com">.</a></li>
+<li><a href="https://www.Twitter.com">.</a></li>
+
+</div>
+
+</footer>
---
+++ conhecendo-ceep/inc/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pr-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conhecendo Ceeppg</title>
+    <link rel="stylesheet" type= "text/css" href="css/Style.css">
+</head>
+<header>
+        <h1 >   <div class= "Tittle"> Conhecendo o 
+                <img class="Ceep" src="img/Ceeppg.png" alt="">
+                </div>
+        </h1>
+        </header>
+</html>
+
+
